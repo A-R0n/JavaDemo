a4091973c106145792dba0b68c6223f8084a275f
A-R0n <aestesc@gmail.com>
2020-03-26 11:52:56 -0500

    Wrapper object auto-boxing

--- src/com/athlete/Climber.java
+++ src/com/athlete/Climber.java
@@ -1,17 +1,28 @@
 package com.athlete;
 
+import java.util.ArrayList;
+
 // Let climber inherit everything that defines an athlete
 public class Climber extends Athlete {
 	// Encapsulate private information into public methods
 	private int gripStrength;
 	private String gripStrengthDescription;
+
 	public Climber(int gripStrength, double routesClimbed) {
 		// Get access to athlete methods with super keyword
 		super(gripStrength);
 		this.gripStrength = gripStrength;
 		setGripStrength(gripStrength);
 	}
-	
+public void climberList() {
+	ArrayList<String> climbers = new ArrayList<String>();
+	climbers.add("Sasha Digulian");
+	climbers.add("Chris Sharma");
+	climbers.add("Alex Megos");
+	climbers.add("Alex Puccio");
+	System.out.println(climbers);
+
+}
 	// Method follows Single Responsibility Principle
 	public int setGripStrength(int num) {
 		return num;
